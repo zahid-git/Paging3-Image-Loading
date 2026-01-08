76f37e222bee3f2fc3ac4a466c8d405863b7db66
Zakhar <hxgdjahdbdnsjsbdbsnsbs@gmail.com>
2026-01-08 20:32:25 +0200

    Fix typo in serialization plugin references in build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serelization)
+    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.ksp)
     id("dagger.hilt.android.plugin")
 }
